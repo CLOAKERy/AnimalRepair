fc660fcd2772f3d311dc30feda08d7dd8eddc784
CLOAKERy <bulochka5544@gmail.com>
2023-06-16 23:54:59 +0300

    заскрафолдил бд

---
+++ AnimalRepair.DAL/Entities/KindOfGender.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Animal_Repair;
+
+public partial class KindOfGender
+{
+    public int Id { get; set; }
+
+    public string Gender { get; set; } = null!;
+
+    public virtual ICollection<Animal> Animals { get; set; } = new List<Animal>();
+}
---
+++ AnimalRepair.DAL/Entities/Order.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Animal_Repair;
+
+public partial class Order
+{
+    public int Id { get; set; }
+
+    public int IdCustomer { get; set; }
+
+    public string Date { get; set; } = null!;
+
+    public double Price { get; set; }
+
+    public virtual ICollection<Animal> Animals { get; set; } = new List<Animal>();
+
+    public virtual Customer IdCustomerNavigation { get; set; } = null!;
+}
