b9d20ebbeebcd8660ee66051c88fad55fe1eff25
CLOAKERy <bulochka5544@gmail.com>
2023-06-24 12:30:22 +0300

    Переместил

--- AnimalRepair.BAL/DTO/KindOfGenderDTO.cs
+++ AnimalRepair.BAL/DTO/KindOfGenderDTO.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace AnimalRepair.DAL.Entities
+namespace AnimalRepair.BLL.DTO
 {
     public class KindOfGenderDTO
     {
