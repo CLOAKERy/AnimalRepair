4c8827169dcfefe5e58a996217f9a2e051696fcd
CLOAKERy <bulochka5544@gmail.com>
2023-06-24 12:27:12 +0300

    Create KindOfGenderDTO.cs

---
+++ AnimalRepair.DAL/Entities/KindOfGenderDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalRepair.DAL.Entities
+{
+    public class KindOfGenderDTO
+    {
+        public int Id { get; set; }
+
+        public string Gender { get; set; } = null!;
+    }
+}
