5b56ba8946f34d699802f93c732a6a95d4bdccbc
CLOAKERy <bulochka5544@gmail.com>
2023-06-24 12:24:26 +0300

    Create OrderProductDTO.cs

---
+++ AnimalRepair.BAL/DTO/OrderProductDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalRepair.BLL.DTO
+{
+    public class OrderProductDTO
+    {
+        public int IdOrder { get; set; }
+
+        public int IdProduct { get; set; }
+    }
+}
