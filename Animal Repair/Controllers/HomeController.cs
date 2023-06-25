3ce9111a9cdf1239dad432ffb8b1ab84343f259b
Yukont <94931609+Yukont@users.noreply.github.com>
2023-06-25 15:50:09 +0300

    add

--- Animal Repair/Controllers/HomeController.cs
+++ Animal Repair/Controllers/HomeController.cs
@@ -25,5 +25,12 @@ namespace Animal_Repair.Controllers
             var animals = mapper.Map<IEnumerable<AnimalDTO>, List<AnimalViewModel>>(AnimalDtos);*/
             return View(AnimalDtos);
         }
+        public async Task<ActionResult> Animal()
+        {
+            IEnumerable<AnimalDTO> AnimalDtos = await animalService.GetAllAnimalsAsync();
+            /*var mapper = new MapperConfiguration(cfg => cfg.CreateMap<AnimalDTO, AnimalViewModel>()).CreateMapper();
+            var animals = mapper.Map<IEnumerable<AnimalDTO>, List<AnimalViewModel>>(AnimalDtos);*/
+            return View(AnimalDtos);
+        }
     }
 }
