5b97ef4590bdf8d6eab4ceef4f2c385d56ff27ff
Yasyuk-zhe <evgenyyasyuk@gmail.com>
2023-06-26 17:21:26 +0300

    Add

--- AnimalRepair.BAL/Services/OrderProductService.cs
+++ AnimalRepair.BAL/Services/OrderProductService.cs
@@ -1,43 +1,69 @@
-﻿using AnimalRepair.BLL.DTO;
+﻿using Animal_Repair;
+using AnimalRepair.BLL.DTO;
+using AnimalRepair.BLL.Infrastructure;
 using AnimalRepair.BLL.Interfaces;
+using AnimalRepair.DAL.Interfaces;
+using AutoMapper;
+using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 namespace AnimalRepair.BLL.Services
 {
-    internal class OrderProductService : IOrderProductService
+    public class OrderProductService : IOrderProductService
     {
-        public Task CreateOrderProduct(OrderProductDTO orderProductDto)
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+        public OrderProductService(IUnitOfWork unitOfWork, IMapper mapper)
         {
-            throw new NotImplementedException();
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+        public async Task CreateOrderProduct(OrderProductDTO orderProductDto)
+        {
+            if (string.IsNullOrEmpty(orderProductDto.IdProduct.ToString()))
+                throw new ValidationException("Поле продукта не может быть пустым", "");
+            if (string.IsNullOrEmpty(orderProductDto.IdOrder.ToString()))
+                throw new ValidationException("Поле заказа не может быть пустым", "");
+
+
+            var orderProduct = _mapper.Map<OrderProductDTO, OrderProduct>(orderProductDto);
+            await _unitOfWork.OrderProducts.CreateAsync(orderProduct);
+            _unitOfWork.Save();
         }
 
         public void Dispose()
         {
-            throw new NotImplementedException();
+            _unitOfWork.Dispose();
         }
 
-        public Task<AnimalDTO> GetOrderProductByIdOrder(int orderId)
+        public async Task RemoveOrderProduct(int orderProductId)
         {
-            throw new NotImplementedException();
+            OrderProduct orderProduct = await _unitOfWork.OrderProducts.GetAsync(orderProductId);
+            if (orderProduct == null)
+                throw new ValidationException("Заказ не найден", "");
+
+            await _unitOfWork.OrderProducts.DeleteAsync(orderProductId);
+            _unitOfWork.Save();
         }
 
-        public Task<AnimalDTO> GetOrderProductByIdProduct(int productId)
+        public async Task UpdateOrderProduct(OrderProductDTO orderProductDto)
         {
-            throw new NotImplementedException();
-        }
+            OrderProduct orderProduct = await _unitOfWork.OrderProducts.GetAsync(orderProductDto.IdOrder);
+            if (orderProduct == null)
+                throw new ValidationException("Заказ не найден", "");
 
-        public Task RemoveOrderProduct(int orderProductId)
-        {
-            throw new NotImplementedException();
-        }
 
-        public Task UpdateOrderProduct(OrderProductDTO orderProductDto)
-        {
-            throw new NotImplementedException();
+            orderProduct.IdOrder = orderProductDto.IdOrder;
+            orderProduct.IdProduct = orderProductDto.IdProduct;
+
+
+            OrderProduct updatedOrderProduct = _mapper.Map<OrderProductDTO, OrderProduct>(orderProductDto);
+
+            await _unitOfWork.OrderProducts.UpdateAsync(updatedOrderProduct);
+            _unitOfWork.Save();
         }
     }
 }
