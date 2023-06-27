b4cfe9653a9ee193affc763a4d47d2c9de80474d
Yukont <94931609+Yukont@users.noreply.github.com>
2023-06-27 20:57:30 +0300

    update

--- AnimalRepair.DAL/Repositories/ProductRepository.cs
+++ AnimalRepair.DAL/Repositories/ProductRepository.cs
@@ -4,15 +4,29 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace AnimalRepair.DAL.Repositories
 {
-    internal class ProductRepository : BaseRepository<Product>
+    internal class ProductRepository : BaseRepository<Product>, IProductRepository<Product>
     {
         public ProductRepository(DbContext dbContext) : base(dbContext)
         {
+        }
+
+        public async Task<IEnumerable<Product>> GetAllAsync(params Expression<Func<Product, object>>[] includes)
+        {
+            IQueryable<Product> query = _dbContext.Set<Product>();
+
+            // Включение связанных данных с помощью метода Include
+            foreach (var include in includes)
+            {
+                query = query.Include(include);
+            }
+
+            return await query.ToListAsync();
         }
     }
 }
