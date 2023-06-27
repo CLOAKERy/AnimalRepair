24dc0979f5f814b176c19a755a8138955a8f2ce1
CLOAKERy <bulochka5544@gmail.com>
2023-06-27 15:53:37 +0300

    Животные

---
+++ Animal Repair/Controllers/AnimalController.cs
@@ -0,0 +1,24 @@
+﻿using AnimalRepair.BLL.DTO;
+using AnimalRepair.BLL.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Animal_Repair.Controllers
+{
+    public class AnimalController : Controller
+    {
+        private readonly ILogger<AnimalController> _logger;
+
+        IAnimalService animalService;
+        public AnimalController(IAnimalService serv, ILogger<AnimalController> logger)
+        {
+            animalService = serv;
+            _logger = logger;
+        }
+        [HttpGet]
+        public async Task<ActionResult> Index()
+        {
+            IEnumerable<AnimalDTO> AnimalDtos = await animalService.GetAllAnimalsAsync();
+            return View(AnimalDtos);
+        }
+    }
+}
