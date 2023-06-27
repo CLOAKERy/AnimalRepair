33bdda55d6b7079919fa1bb571f1cbd3c2bed151
CLOAKERy <bulochka5544@gmail.com>
2023-06-27 19:15:25 +0300

    Update LoginDTO.cs

--- AnimalRepair.BAL/DTO/LoginDTO.cs
+++ AnimalRepair.BAL/DTO/LoginDTO.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,8 +11,14 @@ namespace AnimalRepair.BLL.DTO
     {
         public int Id { get; set; }
 
+        [Required(ErrorMessage = "Укажите имя")]
+        [MaxLength(20, ErrorMessage = "Имя должно иметь длину меньше 20 символов")]
+        [MinLength(3, ErrorMessage = "Имя должно иметь длину больше 3 символов")]
         public string Login1 { get; set; } = null!;
 
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage = "Укажите пароль")]
+        [MinLength(6, ErrorMessage = "Пароль должен иметь длину больше 6 символов")]
         public string Password { get; set; } = null!;
     }
 }
