79036e62521479c013735d97aa4e0e6feadaafad
CLOAKERy <bulochka5544@gmail.com>
2023-06-30 02:15:10 +0300

    Create OrderController.cs

---
+++ Animal Repair/Controllers/OrderController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Animal_Repair.Controllers
+{
+    public class OrderController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
