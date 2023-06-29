35ebf080adce4dfa511e7c1f3868f1c451e1e567
Yasyuk-zhe <evgenyyasyuk@gmail.com>
2023-06-29 15:31:45 +0300

    Add startup

---
+++ Animal Repair/Util/ProductModule.cs
@@ -0,0 +1,14 @@
+﻿using AnimalRepair.BLL.Interfaces;
+using AnimalRepair.BLL.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Animal_Repair.Util
+{
+    public class ProductModule : Controller
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddScoped<IProductService, ProductService>();
+        }
+    }
+}
